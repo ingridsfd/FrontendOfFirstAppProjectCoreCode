ae2d48196acdb0299d2d3ab4a7861076657834fb
ingridsfonoyd@gmail.com <ingridsfonoyd@gmail.com>
2022-10-19 22:20:10 -0500

    finalDetails4

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ function App() {
   const [theme, setTheme] = useState("light");
 
   const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    setTheme((curr) => {
+      document.documentElement.style.setProperty('--bodyColor', curr === "light" ? "#000000" : "#ffffff");
+      return (curr === "light" ? "dark" : "light");
+    });
   };
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}> 
